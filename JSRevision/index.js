7180e4ddf273954f05639f472a0e8723ca403299
PraneethaMuppalla <lakshmimuppalla2453@gmail.com>
2023-12-08 11:35:42 +0530

    Js revision

---
+++ JSRevision/index.js
@@ -0,0 +1,14 @@
+// arrow function
+const productOfTwoNums = (a, b) => a * b;
+
+//student object
+const student = {
+  name: "Praneetha",
+  age: "23",
+  greet() {
+    console.log("hello");
+  },
+};
+
+student.greet();
+console.log(productOfTwoNums(2, 3));
