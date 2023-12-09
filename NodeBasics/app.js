55cc5a934650ce33a5b81220ad0ddd8494a0caec
PraneethaMuppalla <lakshmimuppalla2453@gmail.com>
2023-12-09 18:12:12 +0530

    Reading and writing from file

--- NodeBasics/app.js
+++ NodeBasics/app.js
@@ -1,21 +1,47 @@
 const http = require("http");
-
+const fs = require("fs");
 const server = http.createServer((req, res) => {
   const url = req.url;
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>Home</title></head>");
-  res.write("<body>");
-  if (url === "/home") {
-    res.write("<h1>Welcome to home</h1>");
-  } else if (url === "/about") {
-    res.write("<h1>Welcome to About Us page</h1>");
-  } else if (url === "/node") {
-    res.write("<h1>Welcome to my Node Js project</h1>");
+  const method = req.method;
+
+  if (url === "/") {
+    return fs.readFile("./messages.txt", "utf8", (err, data) => {
+      if (err) {
+        console.log("hit");
+        console.error(err);
+      }
+      res.write("<html>");
+      res.write("<head><title>Node Js</title></head>");
+      res.write("<body>");
+      res.write(`<span>${data}<span>`);
+      res.write(
+        "<form method='POST' action='/msg'><input type='text' name='msg' /><button type='Submit'>Send</button></form>"
+      );
+      res.write("</body>");
+      res.write("</html>");
+      return res.end();
+    });
+  } else if (url === "/msg" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedbody = Buffer.concat(body).toString();
+      const msg = parsedbody.split("=")[1];
+      fs.writeFile("./messages.txt", msg, () => {
+        res.writeHead(302, { Location: "/" });
+        return res.end();
+      });
+    });
   }
-  res.write("</body>");
+  res.write("<html>");
+  res.write("<head><title>Node Js</title></head>");
+  res.write("<body><h1>Welcome to Node Js </h1></body>");
   res.write("</html>");
-  res.end();
+  return res.end();
 });
 
-server.listen(4000);
+server.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
